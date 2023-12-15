804f3dd7bf8543223ade25c0524236b9dffbe203
andres <afquinteromoreano@gmail.com>
2023-12-15 07:46:05 -0500

    Changes package name to tapLock

--- example/app.R
+++ example/app.R
@@ -1,8 +1,9 @@
 library(shiny)
-library(r.sso)
+library(tapLock)
 
 auth_config <- new_openid_config(
-  provider = "google",
+  provider = "entra_id",
+  tenant_id = Sys.getenv("TENANT_ID"),
   client_id = Sys.getenv("CLIENT_ID"),
   client_secret = Sys.getenv("CLIENT_SECRET"),
   app_url = Sys.getenv("APP_URL")
@@ -10,13 +11,6 @@ auth_config <- new_openid_config(
 
 ui <- fluidPage(
   tags$h1("r.sso example"),
-  tags$a(
-    tags$button(
-      "Logout",
-      class = "btn btn-primary"
-    ),
-    href = "/logout"
-  ),
   textOutput("user")
 )
 
@@ -27,7 +21,9 @@ server <- function(input, output, session) {
     family_name <- get_token_field(token(), "family_name")
     expires_at <- expires_at(token())
     glue::glue(
-      "Hello {given_name} {family_name}! Your authenticated session will expire at {expires_at}."
+      "Hello {given_name} {family_name}!",
+      "Your authenticated session will expire at {expires_at}.",
+      .sep = " "
     )
   })
 
